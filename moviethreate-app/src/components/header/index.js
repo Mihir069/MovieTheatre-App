b99d8d9d1b6e82a2d3a10d5f1a824126e70a1e73
AlgoMean143 <108215426+Mihir069@users.noreply.github.com>
2024-02-08 19:00:16 +0530

    make some style

--- moviethreate-app/src/components/header/index.js
+++ moviethreate-app/src/components/header/index.js
@@ -1,3 +1,4 @@
+// index.js
 import { useState } from "react";
 import Logo from "../common/logo";
 import SearchBar from "../common/search-bar";
@@ -6,56 +7,67 @@ import User from "../common/user";
 import Menu from "../common/menu";
 import "./style.css";
 import { Link } from "react-router-dom";
-const MovieHeader = () => {
-    const [menuOpen,setMenuOpen] = useState(false)
 
-    const toggleMenu = () =>{
-        setMenuOpen(!menuOpen)
-    }
+const MovieHeader = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
     return (
         <div className="container">
             <nav className="navbar">
                 <div className="row align-items-center px-sm-3 py-sm-3">
                     <div className="col-12 col-lg-3 cursor">
                         <Link to="/">
-                            <Logo/>
+                            <Logo />
                         </Link>
                     </div>
                     <div className="col-12 col-lg-6 ">
-                        <SearchBar/>
+                        <SearchBar />
                     </div>
                     <div className="col-12 col-lg-3 ">
                         <div className="mx-5 inline-end d-lg-flex">
                             <div className="book-mark">
-                                <BookMark/>
+                                <BookMark />
                             </div>
                             <div className="user">
-                                <User/>
+                                <User />
                             </div>
                             <div className="menu" onClick={toggleMenu}>
-                                <Menu/>
+                                <Menu />
                             </div>
-                            
                         </div>
-
                     </div>
                 </div>
             </nav>
-            {
-                menuOpen && (<div className={`menu-slide ${menuOpen?`open`:''}`}>
-                    <div className="menu-header">
-                        <span onClick={toggleMenu} className="menu-close-icon">X</span>
-                    </div>
-                    <ul className="slider-menu m-2 p-2">
-                        <li className="px-3 menu-links"><Link to="/">About</Link></li>
-                        <li className="px-3"><Link to="/">About</Link></li>
-                        <li className="px-3"><Link to="/">About</Link></li>
-                        <li className="px-3"><Link to="/">About</Link></li>
-                        <li className="px-3"><Link to="/">About</Link></li>
-                    </ul>
+            <div className={`menu-slide ${menuOpen ? "open" : ""}`}>
+                <div className="menu-header">
+                    <span onClick={toggleMenu} className="menu-close-icon">
+                        X
+                    </span>
                 </div>
-            )}
+                <ul className="slider-menu m-2 p-2">
+                    <li className="px-3 menu-links">
+                        <Link to="/">About</Link>
+                    </li>
+                    <li className="px-3">
+                        <Link to="/">About</Link>
+                    </li>
+                    <li className="px-3">
+                        <Link to="/">About</Link>
+                    </li>
+                    <li className="px-3">
+                        <Link to="/">About</Link>
+                    </li>
+                    <li className="px-3">
+                        <Link to="/">About</Link>
+                    </li>
+                </ul>
+            </div>
         </div>
-    )
-}
+    );
+};
+
 export default MovieHeader;
