959d62ea6033ef969ae1f8c5721b28989a999fcd
AlgoMean143 <108215426+Mihir069@users.noreply.github.com>
2024-02-09 20:46:40 +0530

    commit change

--- moviethreate-app/src/components/banner/index.js
+++ moviethreate-app/src/components/banner/index.js
@@ -47,10 +47,7 @@ const MovieBanner = () =>{
                     
                 </div>
                 <div className="slider">
-                    {/* <div className="slider-arrow ">
-                        <button className="p-2" onClick={prevSlide}>&#10094;</button>
-                        <button className="p-2" onClick={nextSlide}>&#10095;</button>
-                    </div> */}
+
                     {
                         banners.map((banner,index)=>(
                             <div key={index} className={`slide ${index === currentSlide?'active':''}`} onClick={()=>goToSlide(index)}>
@@ -65,7 +62,10 @@ const MovieBanner = () =>{
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum.</p>
                     </div>
                 </div>
-
+                    <div className="slider-arrow ">
+                        <button className="p-2" onClick={prevSlide}>&#10094;</button>
+                        <button className="p-2" onClick={nextSlide}>&#10095;</button>
+                    </div>
             </div>
     )
 }
