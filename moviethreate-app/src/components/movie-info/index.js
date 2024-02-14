1127f9234843294016532c37e794a6644825300c
AlgoMean143 <108215426+Mihir069@users.noreply.github.com>
2024-02-14 19:52:55 +0530

    commit change

--- moviethreate-app/src/components/movie-info/index.js
+++ moviethreate-app/src/components/movie-info/index.js
@@ -1,9 +1,9 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-
+import "./style.css";
 const MovieInfo = () => {
     const { movieId } = useParams();
-    const [selectedMovie, setSelectedMovie] = useState(null);
+    const [selectedMovie, setSelectedMovie] = useState([]);
 
     useEffect(() => {
         fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=27e7bd3c69a085aeeb14e90dccf23dfe`, {
@@ -24,7 +24,7 @@ const MovieInfo = () => {
                 };
                 setSelectedMovie(movieData);
             } else {
-                setSelectedMovie(null);
+                setSelectedMovie([]);
             }
         })
         .catch(error => {
@@ -35,13 +35,28 @@ const MovieInfo = () => {
     return (
         <div className="container">
             {selectedMovie && (
-                <div>
-                    <h2>{selectedMovie.title}</h2>
-                    <img src={`https://image.tmdb.org/t/p/w500${selectedMovie.poster}`} alt={selectedMovie.title} />
-                    <p><strong>Release Date:</strong> {selectedMovie.release_date}</p>
-                    <p><strong>Ratings:</strong> {selectedMovie.ratings}</p>
-                    <p><strong>Genres:</strong> {selectedMovie.genres.join(', ')}</p>
-                    <p><strong>Overview:</strong> {selectedMovie.overview}</p>
+                <div className="movie-details-container d-inline-flex my-3 py-3">
+                    <div className="movie-img col-6 d-inline">
+                        <img src={`https://image.tmdb.org/t/p/w500${selectedMovie.poster}`} alt={selectedMovie.title} />
+                    </div>
+                    <div className="movie-details col-6 p-4 d-inline">
+                        <div className="movie-title">
+                            <h2>{selectedMovie.title}</h2>
+                        </div>
+                        <div className="movie-genre">
+                            <p>{selectedMovie.genres}</p>
+                        </div>
+                        <div className="movie-releasing">
+                            <p><strong>Release Date:</strong> {selectedMovie.release_date}</p>
+                        </div>
+                        <div className="movie-ratings">
+                            <p><strong>Ratings:</strong> {selectedMovie.ratings}</p>
+                        </div>
+
+                        <div className="movie-overview">
+                            <p><strong>Overview:</strong> {selectedMovie.overview}</p>
+                        </div>
+                    </div>
                 </div>
             )}
             {!selectedMovie && <p>Loading...</p>}
