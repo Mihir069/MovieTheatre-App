acafb4abc501308fb574f5bf6c782ccb7f49102c
AlgoMean143 <108215426+Mihir069@users.noreply.github.com>
2024-02-16 20:08:31 +0530

    added search

--- moviethreate-app/src/components/common/search-bar/index.js
+++ moviethreate-app/src/components/common/search-bar/index.js
@@ -42,8 +42,11 @@ const SearchBar = () => {
                 {movieSearched.map((movie) => (
                     <div key={movie.id} className="row">
                         <Link to={`/movie/${movie.id}`} onClick={handleMovieClick}>
-                        <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title}/>
-                        <h6 className="col-6">{movie.title}</h6>
+                            <div className="d-inline-flex  mx-1 px-1">
+                                <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} className="col-auto"/>
+                                <h6 className="col-auto title my-5 mx-4">{movie.title}</h6>
+                            </div>
+
                         </Link>
 
                     </div>
