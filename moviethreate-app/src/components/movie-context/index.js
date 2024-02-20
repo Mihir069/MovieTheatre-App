556b34fce4b0ce09e7109ab4349e641943a73670
AlgoMean143 <108215426+Mihir069@users.noreply.github.com>
2024-02-20 19:15:02 +0530

    changed in fetch

--- moviethreate-app/src/components/movie-context/index.js
+++ moviethreate-app/src/components/movie-context/index.js
@@ -9,139 +9,158 @@ const MovieProvider = ({children})=>{
     const [topRates,setTopRates] = useState([]);
     const [trendingMovies,setTrendingMovies] = useState([]);
 
-    useEffect(()=>{
-        fetch(`https://api.themoviedb.org/3/trending/all/day?api_key=27e7bd3c69a085aeeb14e90dccf23dfe`,{
-            method:'GET',
-        })
-        .then(res=>res.json())
-        .then(data=>{
-            console.log(data);
-            if(data.results){
-                const trendingmovieList = data.results.map(items=>({
-                    id:items.id,
-                    name: items.original_title,
-                    poster:items.backdrop_path,
-                    release_date:items.release_date,
-                    ratings:items.vote_average,
-                    genre: items.genre_ids
-                }));
-                setTrendingMovies(trendingmovieList);
-            }else{
-                console.log("no movie")
-                setTrendingMovies([])
-            }
-        })
-    },[]);
+    const [fetchedData,setFetchedData] = useState(false);
 
     useEffect(()=>{
-        fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=27e7bd3c69a085aeeb14e90dccf23dfe`,{
-            method:'GET',
-        })
-        .then(res=>res.json())
-        .then(data=>{
-            console.log(data);
-            if(data.results){
-                const movieList = data.results.map(items=>({
-                    id:items.id,
-                    name: items.original_title,
-                    poster:items.backdrop_path,
-                    release_date:items.release_date,
-                    ratings:items.vote_average,
-                    genre: items.genre_ids
-                }));
-                setMovies(movieList);
-            }else{
-                console.log("no movie")
-                setMovies([])
-            }
-        })
-    },[]);
+        if(!fetchedData){
+            fetch(`https://api.themoviedb.org/3/trending/all/day?api_key=27e7bd3c69a085aeeb14e90dccf23dfe`,{
+                method:'GET',
+            })
+            .then(res=>res.json())
+            .then(data=>{
+                console.log(data);
+                if(data.results){
+                    const trendingmovieList = data.results.map(items=>({
+                        id:items.id,
+                        name: items.original_title,
+                        poster:items.backdrop_path,
+                        release_date:items.release_date,
+                        ratings:items.vote_average,
+                        genre: items.genre_ids
+                    }));
+                    setTrendingMovies(trendingmovieList);
+                }else{
+                    console.log("no movie")
+                    setTrendingMovies([])
+                }
+            })
+        }
+
+    },[fetchedData]);
 
     useEffect(()=>{
-        fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=27e7bd3c69a085aeeb14e90dccf23dfe`,{
-            method:'GET'
-        })
-        .then(res=>res.json())
-        .then(data=>{
-            if(data.results){
-                const playingMovie = data.results.map(items=>({
-                    id:items.id,
-                    name: items.original_title,
-                    poster:items.backdrop_path,
-                    release_date:items.release_date,
-                    ratings:items.vote_average,
-                    genre: items.genre_ids
-                }));
-                setPlayingMovies(playingMovie)
-            }else{
-                setPlayingMovies([])
-            }
-        })
-    },[])
+        if(!fetchedData){
+            fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=27e7bd3c69a085aeeb14e90dccf23dfe`,{
+                method:'GET',
+            })
+            .then(res=>res.json())
+            .then(data=>{
+                console.log(data);
+                if(data.results){
+                    const movieList = data.results.map(items=>({
+                        id:items.id,
+                        name: items.original_title,
+                        poster:items.backdrop_path,
+                        release_date:items.release_date,
+                        ratings:items.vote_average,
+                        genre: items.genre_ids
+                    }));
+                    setMovies(movieList);
+                }else{
+                    console.log("no movie")
+                    setMovies([])
+                }
+            })
+        }
+
+    },[fetchedData]);
 
     useEffect(()=>{
-        fetch(`https://api.themoviedb.org/3/movie/popular?api_key=27e7bd3c69a085aeeb14e90dccf23dfe`,{
-            method:'GET'
-        })
-        .then(res=>res.json())
-        .then(data=>{
-            if(data.results){
-                const populer = data.results.map(items=>({
-                    id:items.id,
-                    name: items.original_title,
-                    poster:items.backdrop_path,
-                    release_date:items.release_date,
-                    ratings:items.vote_average,
-                    genre: items.genre_ids
-                }));
-                setPopulerMovie(populer)
-            }else{
-                setPopulerMovie([])
-            }
-        })
-    },[])
+        if(!fetchedData){
+            fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=27e7bd3c69a085aeeb14e90dccf23dfe`,{
+                method:'GET'
+            })
+            .then(res=>res.json())
+            .then(data=>{
+                if(data.results){
+                    const playingMovie = data.results.map(items=>({
+                        id:items.id,
+                        name: items.original_title,
+                        poster:items.backdrop_path,
+                        release_date:items.release_date,
+                        ratings:items.vote_average,
+                        genre: items.genre_ids
+                    }));
+                    setPlayingMovies(playingMovie)
+                }else{
+                    setPlayingMovies([])
+                }
+            })
+        }
+
+    },[fetchedData])
 
     useEffect(()=>{
-        fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=27e7bd3c69a085aeeb14e90dccf23dfe`,{
-            method:'GET'
-        })
-        .then(res=>res.json())
-        .then(data=>{
-            if(data.results){
-                const topMovies = data.results.map(items=>({
-                    id:items.id,
-                    name: items.original_title,
-                    poster:items.backdrop_path,
-                    release_date:items.release_date,
-                    ratings:items.vote_average,
-                    genre: items.genre_ids
-                }));
-                setTopRates(topMovies)
-            }else{
-                setTopRates([])
-            }
-        })
-    },[])
+        if(!fetchedData){
+            fetch(`https://api.themoviedb.org/3/movie/popular?api_key=27e7bd3c69a085aeeb14e90dccf23dfe`,{
+                method:'GET'
+            })
+            .then(res=>res.json())
+            .then(data=>{
+                if(data.results){
+                    const populer = data.results.map(items=>({
+                        id:items.id,
+                        name: items.original_title,
+                        poster:items.backdrop_path,
+                        release_date:items.release_date,
+                        ratings:items.vote_average,
+                        genre: items.genre_ids
+                    }));
+                    setPopulerMovie(populer)
+                }else{
+                    setPopulerMovie([])
+                }
+            })
+        }
+    },[fetchedData])
 
     useEffect(()=>{
-        fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=27e7bd3c69a085aeeb14e90dccf23dfe`,{
-            method:'GET'
-        })
-        .then(res=>res.json())
-        .then(data=>{
-            console.log(data);
-            if(data.genres){
-                const movieGenre = data.genres.map(items=>({
-                    id:items.id,
-                    genre_name:items.name
-                }));
-                setMovieGenre(movieGenre);
-            }else{
-                console.log("Genre is not availabe")
-            }
+        if(!fetchedData){
+            fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=27e7bd3c69a085aeeb14e90dccf23dfe`,{
+                method:'GET'
+            })
+            .then(res=>res.json())
+            .then(data=>{
+                if(data.results){
+                    const topMovies = data.results.map(items=>({
+                        id:items.id,
+                        name: items.original_title,
+                        poster:items.backdrop_path,
+                        release_date:items.release_date,
+                        ratings:items.vote_average,
+                        genre: items.genre_ids
+                    }));
+                    setTopRates(topMovies)
+                }else{
+                    setTopRates([])
+                }
+            })
+        }
 
-        })
-    },[])
+    },[fetchedData])
+
+    useEffect(()=>{
+        if(fetchedData){
+            fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=27e7bd3c69a085aeeb14e90dccf23dfe`,{
+                method:'GET'
+            })
+            .then(res=>res.json())
+            .then(data=>{
+                console.log(data);
+                if(data.genres){
+                    const movieGenre = data.genres.map(items=>({
+                        id:items.id,
+                        genre_name:items.name
+                    }));
+                    setMovieGenre(movieGenre);
+                }else{
+                    console.log("Genre is not availabe")
+                }
+    
+            })
+        }
+
+    },[fetchedData])
     return(
         <MovieContext.Provider value={{movies,movieGenre,playingMovies,populerMovie,topRates,trendingMovies}}>
             {children}
