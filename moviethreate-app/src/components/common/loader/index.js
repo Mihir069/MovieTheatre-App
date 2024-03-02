0d9ed422e1f3a06f9f0ff0d7493fe044d45ccd42
AlgoMean143 <108215426+Mihir069@users.noreply.github.com>
2024-03-02 17:20:28 +0530

    added loader spinner

---
+++ moviethreate-app/src/components/common/loader/index.js
@@ -0,0 +1,11 @@
+import "./style.css";
+const Loading = (loader) => {
+    return (
+        <div className="spinner-container m-5 p-5">
+            <span class="loader"></span>
+        </div>
+
+    );
+};
+
+export default Loading;
--- moviethreate-app/src/page/home/index.js
+++ moviethreate-app/src/page/home/index.js
@@ -5,8 +5,17 @@ import PopulerMovies from "../../components/populer-movies";
 import TopRatedMovies from "../../components/top-movies";
 import GenreList from "../../components/genre-list";
 import TrendingMovies from "../../components/trendings";
+import Loading from "../../components/common/loader";
+import { useState } from "react";
 
 const Home = () =>{
+    const [loading,setLoading] = useState(true)
+    setTimeout(()=>setLoading(false),2000);
+    if(loading){
+        return(
+            <Loading/>
+        )
+    }
     return(
         <>
             <MovieBanner/>
