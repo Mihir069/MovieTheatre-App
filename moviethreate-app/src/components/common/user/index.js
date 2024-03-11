2b0e2410cd0e92d19c09d39c69aab64a00dc480a
AlgoMean143 <108215426+Mihir069@users.noreply.github.com>
2024-03-11 18:18:49 +0530

    update moviethreate-app/src/components/common/user/index.js

--- moviethreate-app/src/components/common/user/index.js
+++ moviethreate-app/src/components/common/user/index.js
@@ -1,10 +1,22 @@
 import { Link } from "react-router-dom";
 
-const User = () =>{
-    return(
-        <Link to={`/account`}>
-        <img src="./svg/user-solid.svg" alt="user" height="20px" className=" cursor mx-2" />
-        </Link>
-    )
+const User = ({isLoggedIn}) =>{
+    // return(
+        
+    //     <Link to={`/account`}>
+    //     <img src="./svg/user-solid.svg" alt="user" height="20px" className=" cursor mx-2" />
+    //     </Link>
+    // )
+    return (
+        <div>
+          {isLoggedIn ? (
+            <img src="existing_user_image_url" alt="user" height="20px" className="cursor mx-2" />
+          ) : (
+            <Link to="/login">
+              <img src="./svg/user-solid.svg" alt="user" height="20px" className="cursor mx-2" />
+            </Link>
+          )}
+        </div>
+      );
 }
 export default User;
