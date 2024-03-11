632c95e76f83a00ae414bdf434ee38f8d35d8aaa
AlgoMean143 <108215426+Mihir069@users.noreply.github.com>
2024-03-11 20:58:56 +0530

    changed in reducers

--- moviethreate-app/src/page/accountpage/index.js
+++ moviethreate-app/src/page/accountpage/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import './style.css'; 
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchUserApi } from '../../services';
