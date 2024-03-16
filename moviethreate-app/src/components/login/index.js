b85e27466558f2e3b310d8c320c5a0eeb57a0d16
AlgoMean143 <108215426+Mihir069@users.noreply.github.com>
2024-03-16 22:10:10 +0530

    update moviethreate-app/src/components/login/index.js and moviethreate-app/src/page/accountpage/index.js

--- moviethreate-app/src/components/login/index.js
+++ moviethreate-app/src/components/login/index.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { Navigate } from 'react-router-dom';
-import AccountPage from '../../page/accountpage';
 import "./style.css"
 
 const LoginPage = () => {
