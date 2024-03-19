3b4d71f744c59fcb4c3e546a67a1b8092538a42d
AlgoMean143 <108215426+Mihir069@users.noreply.github.com>
2024-03-19 17:54:09 +0530

    update moviethreate-app/src/config/index.js

--- moviethreate-app/src/config/index.js
+++ moviethreate-app/src/config/index.js
@@ -1,4 +1,4 @@
 export const apiURL = 'https://api.themoviedb.org/3/';
 export const searchAPI = 'search/movie?api_key=27e7bd3c69a085aeeb14e90dccf23dfe';
 export const keyAPI = `?api_key=27e7bd3c69a085aeeb14e90dccf23dfe`;
-// "https://api.themoviedb.org/3/account/20960400?api_key=27e7bd3c69a085aeeb14e90dccf23df"
+
