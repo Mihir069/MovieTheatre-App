2c327fb0ab8c8c03eba6cef61296c40a47e2930a
AlgoMean143 <108215426+Mihir069@users.noreply.github.com>
2024-03-24 16:44:01 +0530

    update firebase.js and moviethreate-app/src/index.css

--- moviethreate-app/src/firebase.js
+++ moviethreate-app/src/firebase.js
@@ -1,21 +1,21 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-export const firebaseConfig = {
-  apiKey: "AIzaSyCWZxJxp1zB42IJgS9QbZQrUrdSDLnu2a0",
-  authDomain: "movie-theatre-b668c.firebaseapp.com",
-  projectId: "movie-theatre-b668c",
-  storageBucket: "movie-theatre-b668c.appspot.com",
-  messagingSenderId: "927783295195",
-  appId: "1:927783295195:web:5f319d71882cd1554954fe",
-  measurementId: "G-E8K8T4RF3V"
-};
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// export const firebaseConfig = {
+//   apiKey: "AIzaSyCWZxJxp1zB42IJgS9QbZQrUrdSDLnu2a0",
+//   authDomain: "movie-theatre-b668c.firebaseapp.com",
+//   projectId: "movie-theatre-b668c",
+//   storageBucket: "movie-theatre-b668c.appspot.com",
+//   messagingSenderId: "927783295195",
+//   appId: "1:927783295195:web:5f319d71882cd1554954fe",
+//   measurementId: "G-E8K8T4RF3V"
+// };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
